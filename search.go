3f6af1108001ec03c0286f62e7af936ec6e39e5d
Dave Nelson <davidicus21@gmail.com>
2015-05-16 16:38:38 -0600

    checks search 'type'

--- search.go
+++ search.go
@@ -6,6 +6,9 @@ import (
 	"strings"
 )
 
+var acceptableParams = []string{"artist", "track", "album"}
+var acceptableTypes = []string{"artist", "track", "album", "playlist"}
+
 // Accept a map of search params (e.g track name, artist name, playlist name, etc) and
 // search type slice [artist, playlist, track]. Types can be any number of types acceptable by api, must  have >= one.
 func HardSearch(params map[string]string, itemTypes []string) (searchUrl string, err error) {
@@ -13,9 +16,19 @@ func HardSearch(params map[string]string, itemTypes []string) (searchUrl string,
 	// ensure keys of params are all Spotify field filters
 	var passes bool
 	for k, _ := range params {
-		passes = checkParam(k)
+		passes = checkParam(k, acceptableParams)
 		if passes == false {
 			err := errors.New("HardSearch can only search for artist, track, or album. Found:" + k)
+			return "", err
+		}
+	}
+
+	// ensure types are compatible
+	passes = true
+	for _, v := range itemTypes {
+		passes = checkParam(v, acceptableTypes)
+		if passes == false {
+			err := errors.New("HardSearch can only search by type artist, track, album, or playlist. Found:" + v)
 			return "", err
 		}
 	}
@@ -32,11 +45,9 @@ func HardSearch(params map[string]string, itemTypes []string) (searchUrl string,
 
 // TODO: Soft search
 
-func checkParam(param string) bool {
+func checkParam(param string, check []string) bool {
 
-	var acceptableParams = []string{"artist", "track", "album"}
-
-	for _, p := range acceptableParams {
+	for _, p := range check {
 		if param == p {
 			return true
 		}
