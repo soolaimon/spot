ba1819bc1279738edbe81275db5d554be58761a0
Dave Nelson <davidicus21@gmail.com>
2015-05-30 00:07:49 -0600

    forces order when looping through params for predictability, consistent test passing

--- search.go
+++ search.go
@@ -3,6 +3,7 @@ package spot
 import (
 	"errors"
 	"net/url"
+	"sort"
 	"strings"
 )
 
@@ -13,6 +14,7 @@ var acceptableTypes = []string{"artist", "track", "album", "playlist"}
 // search type slice [artist, playlist, track]. Types can be any number of types acceptable by api, must  have >= one.
 func BuildHardUrl(params map[string]string, itemTypes []string) (searchUrl string, err error) {
 
+	var orderer []string
 	// ensure keys of params are all Spotify field filters
 	var passes bool
 	for k, _ := range params {
@@ -21,6 +23,8 @@ func BuildHardUrl(params map[string]string, itemTypes []string) (searchUrl strin
 			err := errors.New("HardSearch can only search for artist, track, or album. Found: " + k)
 			return "", err
 		}
+		// while we're add it, create our array of param types for ordering below
+		orderer = append(orderer, k)
 	}
 
 	// ensure types are compatible
@@ -35,9 +39,12 @@ func BuildHardUrl(params map[string]string, itemTypes []string) (searchUrl strin
 
 	queryString := ""
 
+	//	order params for predictability
+	sort.Strings(orderer)
+
 	// format params into spotify search query (q=track:Track+Name+artist:Artist+Name)
-	for k, v := range params {
-		queryString += url.QueryEscape(strings.ToLower(k)) + ":" + url.QueryEscape(strings.ToLower(v)) + "+"
+	for _, t := range orderer {
+		queryString += url.QueryEscape(strings.ToLower(t)) + ":" + url.QueryEscape(strings.ToLower(params[t])) + "+"
 	}
 
 	return "https://api.spotify.com/v1/search?q=" + queryString + "&type=" + strings.Join(itemTypes, ","), err
