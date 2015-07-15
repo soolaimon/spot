066c35ace8a1b5f009d0f25fa84481220e5fb791
Dave Nelson <davidicus21@gmail.com>
2015-07-14 21:45:59 -0600

    adds album attributes

--- album.go
+++ album.go
@@ -1,7 +1,15 @@
 package spot
 
 type Album struct {
-	SpotId string
+	AvailableMarkets []string
+	Artists          []Artist
+	spotId           string `json:"id"`
+	Uri              string
+	Genres           []string
+	Name             string
+	ReleaseDate      string `json:"release_date"`
+	ReleasePrecision string `json:"release_date_precision"`
+	Tracks           TrackResults
 }
 
 type AlbumResults struct {
